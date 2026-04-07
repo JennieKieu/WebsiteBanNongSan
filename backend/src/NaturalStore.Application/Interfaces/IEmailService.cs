09eac9cc03537ec960710180c84822072eae7dce
JennieKieu <140492352+JennieKieu@users.noreply.github.com>
2026-04-07 07:09:04 +0700

    Delete backend/src directory

--- backend/src/NaturalStore.Application/Interfaces/IEmailService.cs
+++
@@ -1,6 +0,0 @@
-namespace NaturalStore.Application.Interfaces;
-
-public interface IEmailService
-{
-    Task SendOtpAsync(string toEmail, string otp, string type, CancellationToken ct = default);
-}
